e9fba41229c28833157a9b8d0980aa75ffe70ea0
Rodrigo borges <rbgameslinux@gmail.com>
2025-04-20 23:59:49 -0300

    Update instalação_GPU_AMD.sh
    adiciana dependencia qt5ct-kde

--- instalação_GPU_AMD.sh
+++ instalação_GPU_AMD.sh
@@ -13,8 +13,8 @@ Error='[\033[1;31mERRO\033[0m]'
 usuario=$(whoami)
 package_amd=(lib32-vulkan-radeon amd-ucode xf86-video-amdgpu xf86-video-ati vulkan-radeon)
 package_intel=(vulkan-intel intel-ucode libva-intel-driver xf86-video-intel lib32-vulkan-intel)
-package_yay_list=(swayosd-git waypaper qt6ct-kde grimblast swaylock-effects reiserfsprogs wlogout protonup-qt-bin clipman clipse heroic-games-launcher-bin visual-studio-code-bin)   
-packages=(base-devel git wget unzip bash-completion qt5ct fish gum rofi wofi unrar okular waybar swww dolphin dolphin-plugins ark firefox loupe xdg-desktop-portal xdg-desktop-portal-gnome xdg-desktop-portal-gtk
+package_yay_list=(swayosd-git waypaper qt5ct-kde qt6ct-kde grimblast swaylock-effects reiserfsprogs wlogout protonup-qt-bin clipman clipse heroic-games-launcher-bin visual-studio-code-bin)   
+packages=(base-devel git wget unzip bash-completion fish gum rofi wofi unrar okular waybar swww dolphin dolphin-plugins ark firefox loupe xdg-desktop-portal xdg-desktop-portal-gnome xdg-desktop-portal-gtk
     xdg-desktop-portal-hyprland blueman bluedevil xdg-desktop-portal-wlr xdg-user-dirs xdg-user-dirs-gtk xdg-utils archlinux-xdg-menu btop ttf-font-awesome noto-fonts noto-fonts-emoji
     noto-fonts-extra ttf-firacode-nerd ttf-jetbrains-mono-nerd swayidle polkit-gnome bash-completion ntfs-3g ffmpegthumbnailer ffmpegthumbs volumeicon pavucontrol pamixer notification-daemon 
     wl-clipboard wayland-utils clinfo alsa-utils  imagemagick adw-gtk-theme breeze breeze5 breeze-gtk breeze-icons nwg-look bluez bluez-utils bluez-tools kio kde-cli-tools sddm xorg
@@ -98,6 +98,8 @@ grep -q '^#\[multilib\]' "${PacmanPath}" && {
     # instalação de pacotes
 
 sudo pacman -Syu --noconfirm --needed
+cd $HOME/pos_install_hyprland
+sudo pacman -U qqc2-desktop-style5-5.116.1-1-x86_64.pkg.tar.zst
 
 echo "Instalando pacotes necessarios do systema com pacman"
 
